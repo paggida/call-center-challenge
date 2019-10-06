a59534743d679614021110bf81f9c810b719a309
paggida <danilopaggi@gmail.com>
2019-10-06 13:43:07 -0300

    DataBase Feature

---
+++ API/__tests__/unity/databaseFunctions.test.js
@@ -0,0 +1,83 @@
+const db = require('../../src/app/functions/databaseFunctions');
+
+describe('Insert function validation', () => {
+  it('should not be able to insert record in a inexisting table', async () => {
+    const response = await db.insert(
+      { id: 99999999, status: 'call.new' },
+      'TB_X'
+    );
+    expect(response.status).toBe(1);
+    expect(response.message).toBe('Table not found.');
+  });
+  it('should not be able to insert an invalid record', async () => {
+    const response = await db.insert({}, 'TB_CUSTOMERS');
+    expect(response.status).toBe(2);
+    expect(response.message).toBe('Invalid record.');
+  });
+  it('should be able to insert a valid record', async () => {
+    const response = await db.insert(
+      { id: 99999999, status: 'call.new' },
+      'TB_CUSTOMERS'
+    );
+    expect(response.status).toBe(0);
+  });
+  it('should not be able to insert an id with duplicity', async () => {
+    const response = await db.insert(
+      { id: 99999999, status: 'call.new' },
+      'TB_CUSTOMERS'
+    );
+    expect(response.status).toBe(3);
+    expect(response.message).toBe('Id with duplicity.');
+  });
+});
+
+describe('Update function validation', () => {
+  it('should not be able to update record in a inexisting table', async () => {
+    const response = await db.update(
+      { id: 99999999, status: 'call.new' },
+      'TB_X'
+    );
+    expect(response.status).toBe(1);
+    expect(response.message).toBe('Table not found.');
+  });
+  it('should not be able to update an invalid record', async () => {
+    const response = await db.update({}, 'TB_CUSTOMERS');
+    expect(response.status).toBe(2);
+    expect(response.message).toBe('Invalid record.');
+  });
+  it('should not be able to update a inexistent record', async () => {
+    const response = await db.update(
+      { id: 11111111, status: 'call.waiting' },
+      'TB_CUSTOMERS'
+    );
+    expect(response.status).toBe(4);
+  });
+  it('should be able to update a valid record', async () => {
+    const response = await db.update(
+      { id: 99999999, status: 'call.waiting' },
+      'TB_CUSTOMERS'
+    );
+    expect(response.status).toBe(0);
+  });
+});
+
+describe('Find by id validation', () => {
+  it('should be able to find an existing record', async () => {
+    const response = await db.findById(99999999, 'TB_CUSTOMERS');
+    expect(response.id).toBe(99999999);
+    expect(response.status).toBe('call.waiting');
+  });
+  it('should not be able to find an inexisting record', async () => {
+    const response = await db.findById(11111111, 'TB_CUSTOMERS');
+    expect(response).toBeUndefined();
+  });
+});
+
+describe('Validation of record delete ', () => {
+  it('should be able to delete an existing record', async () => {
+    const response = await db.delete(99999999, 'TB_CUSTOMERS');
+    const searchObject = await db.findById(99999999, 'TB_CUSTOMERS');
+    expect(response.status).toBe(0);
+    expect(searchObject).toBeUndefined();
+  });
+});
--- API/src/app/functions/databaseFunctions.js
+++ API/src/app/functions/databaseFunctions.js
@@ -1,10 +1,125 @@
+const fileReader = require('fs');
+const { promisify } = require('util');
+const error = require('../Exceptions/databaseExceptions');
+
 module.exports = {
-  async insert(record, table) {},
-  update(key, record, table) {},
-  find(key, table) {},
-  list(table) {}
+  async findById(key, table) {
+    const tableObject = await _getcontentsTable(table);
+    return tableObject.find(record => record.id === key);
+  },
+  async listActiveCalls(table) {
+    const tableObject = await _getcontentsTable(table);
+    return tableObject.filter(record => record.status !== 'call.finished');
+  },
+  async insert(record, table) {
+    if (await _isExistTable(table)) {
+      if (await _isValidRecord(record, table)) {
+        // If it's a new Id...
+        if (!(await this.findById(record.id, table))) {
+          const tableObject = await _getcontentsTable(table);
+          tableObject.push(record);
+          if (await _setcontentsTable(tableObject, table)) {
+            return { status: 0 };
+          } else {
+            return error.throwException(5);
+          }
+        } else {
+          return error.throwException(3);
+        }
+      } else {
+        return error.throwException(2);
+      }
+    } else {
+      return error.throwException(1);
+    }
+  },
+  async update(record, table) {
+    if (await _isExistTable(table)) {
+      if (await _isValidRecord(record, table)) {
+        // If it's an existing Id...
+        if (!!(await this.findById(record.id, table))) {
+          const tableObject = await _getcontentsTable(table);
+          const newTableObject = tableObject.map(item => {
+            return item.id === record.id ? record : item;
+          });
+
+          if (await _setcontentsTable(newTableObject, table)) {
+            return { status: 0 };
+          } else {
+            return error.throwException(5);
+          }
+        } else {
+          return error.throwException(4);
+        }
+      } else {
+        return error.throwException(2);
+      }
+    } else {
+      return error.throwException(1);
+    }
+  },
+  async delete(key, table) {
+    const tableObject = await _getcontentsTable(table);
+    const newTableObject = tableObject.filter(record => record.id !== key);
+    if (await _setcontentsTable(newTableObject, table)) {
+      return { status: 0 };
+    } else {
+      return error.throwException(5);
+    }
+  }
 };
 
-isExistTable = table => {};
+_isExistTable = async table =>
+  await _isExistFile(`./src/database/${table}.json`);
 
-isValidRecord = (record, schema) => {};
+_isValidRecord = async (record, table) => {
+  const schema = `./src/database/schemas/${table}_SCHEMA.json`;
+  if (!_isEmptyObject(record)) {
+    // Validate the existence of a schema for the table.
+    if (await _isExistFile(schema)) {
+      const schemaStructure = await _openJsonFile(schema);
+      return !_thereAreMissingAtributes(record, schemaStructure);
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
+
+_getcontentsTable = async table =>
+  await _openJsonFile(`./src/database/${table}.json`);
+
+_setcontentsTable = async (newContents, table) =>
+  await promisify(fileReader.writeFile)(
+    `./src/database/${table}.json`,
+    JSON.stringify(newContents)
+  )
+    .then(() => true)
+    .catch(() => false);
+
+_openJsonFile = async file =>
+  await promisify(fileReader.readFile)(file)
+    .then(content => JSON.parse(content))
+    .catch(() => '');
+
+/* return new Promise((resolve, reject) => {
+    fileReader.readFile(file, (err, data) => {
+      if (err) resolve({});
+      resolve(JSON.parse(data));
+    });
+  }); */
+
+_isExistFile = async file =>
+  await promisify(fileReader.stat)(file)
+    .then(() => true)
+    .catch(() => false);
+
+_isEmptyObject = object => Object.keys(object).length === 0;
+
+_thereAreMissingAtributes = (record, schema) => {
+  const missingAttributes = Object.keys(schema).filter(
+    attributes => Object.keys(record).indexOf(attributes) === -1
+  );
+  return missingAttributes.length === 0 ? false : true;
+};
