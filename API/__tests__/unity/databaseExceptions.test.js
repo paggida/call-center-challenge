b29b422f01871cfe12a46593a038e1a451741267
Danilo Paggi <Danilo.Paggi@arisp.com.br>
2019-10-09 09:26:37 -0300

    Adding database tests

--- API/__tests__/unity/databaseExceptions.test.js
+++ API/__tests__/unity/databaseExceptions.test.js
@@ -4,11 +4,13 @@ describe('Validation databaseExceptions.js', () => {
   it('should be able to send a known error code', () => {
     const response = error.throwException(1);
     expect(response.status).toBe(1);
+    expect(response.type).toBe('DB');
     expect(response.message).toBe('Table not found.');
   });
   it('should not throw an exception if it receives an unknown error code.', () => {
     const response = error.throwException('X');
     expect(response.status).toBe('X');
+    expect(response.type).toBe('DB');
     expect(response.message).toBe('Unknown error code');
   });
 });
