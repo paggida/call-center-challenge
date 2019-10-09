9e5a937a9ec3791843cdc988c6dcb8e7535a4ca0
Danilo Paggi <Danilo.Paggi@arisp.com.br>
2019-10-09 15:37:44 -0300

    Constant configuration

---
+++ API/src/app/config/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //----------Table constants-------------
+  TABLE_CUSTOMERS: 'TB_CUSTOMERS',
+  TABLE_ERROR_LOG: 'TB_ERROR_LOG',
+  //----------Branch constants-------------
+  FIRST_CONTACT_BRANCH: '900',
+  COMMON_CONTACT_BRANCH: '901',
+  //----------Status constants------------
+  STANDBY_CALL_STATUS: 2
+};
